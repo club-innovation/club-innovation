6b7c488067bf71b54ad25698f06f9fd1c989bfdb
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-02-16 17:18:02 +0100

    add a simple navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,27 @@
 import React from "react"
 import "./Navbar.css"
 
+import { Outlet, Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
   )
 }
 
