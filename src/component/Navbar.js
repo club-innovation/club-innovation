f69b0acf7e74febd8373e157e608fd85a643d62b
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-02-18 00:30:00 +0100

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -12,10 +12,16 @@ function Navbar() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/blogs">Blogs</Link>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
           </li>
           <li>
             <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
