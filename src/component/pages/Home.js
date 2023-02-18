0723e117fa86fcf4776373565245bf674fea86d1
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-02-18 18:39:32 +0100

    Home page
    added the background picture and the welcome and description

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -4,9 +4,21 @@ import background from "../../imgs/home/background.jpg"
 
 function Home() {
   return (
-    <div className="background">
-      <img src={background} alt="Background"/>
+    <><div className="background">
+    
+   <img src={background} alt="Background" />
+  <nav className="nav1">
+  <ul className="ul1">
+   <li className="p1"> welcome <span className="to">To</span></li>
+   <li className="p2">&#123;Club<span className="innovation">Innovation</span>&#125;</li>
+    <li className="par1">Club Innovation registration is open!</li>
+   <li className="par2">Click the button below to register.</li>
+   </ul></nav>
     </div>
+    <div className="sign in">
+ 
+     
+      </div></>
   )
 }
 
