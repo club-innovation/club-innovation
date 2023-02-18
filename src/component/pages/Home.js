b819a23bb9d463fe291be5097226a5b3a2d94ab2
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-02-18 20:37:06 +0100

    Home page update
    i ll have to align the text to be in the right .
    cleaning the html and css is done.

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -4,20 +4,13 @@ import background from "../../imgs/home/background.jpg"
 
 function Home() {
   return (
-    <><div className="background">
-    
-   <img src={background} alt="Background" />
-
-   
-    </div>
-    <div className="discription">
- <p className="p1"> Welcome <span className="to">To</span></p>
-   <p className="p2">&#123;Club<span className="innovation">Innovation</span>&#125;</p>
-    <p className="par1">Club Innovation registration is open!</p>
-   <p className="par2">Click the button below to register.</p>
-
-     
-      </div></>
+  <div className="background">  
+      <div className="discription">
+          <p className="p1"> Welcome <span className="primary-color">To</span></p>
+          <p className="p2">&#123;Club<span className="primary-color">Innovation</span>&#125;</p>
+      </div>
+  </div>
+  
   )
 }
 
