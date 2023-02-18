1f3bf3bc36a2c7f49731721a6cb20c71f5ba4139
taha-ammour <122755276+taha-ammour@users.noreply.github.com>
2023-02-18 21:19:39 +0100

    changed footer
    added the first changes on footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,21 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <div>Footer 2023</div>
+    <> 
+       <div className="Footer">
+        <div className="container1">
+          <div className="parag">
+            <p className="p1">Want to become part of our community?</p>
+            <p className="p2">Get in touch</p>
+            <p className="p3"><h1>&#123;Club<span className="secondary-color">Innovation</span>&#125;</h1></p>
+            <p className="p4">Want to be part of Us?</p>
+            <p className="p5">Check out our game challenge</p>
+          </div>
+        </div>
+       </div>
+
+    </>
+
   )
 }
 
