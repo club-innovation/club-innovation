b0ec1de79b47009a8a3d190413e9a2176611392f
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-02-19 20:01:35 +0100

    fix responsive navbar in mobile

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./Navbar.css"
 import closebtn from "../imgs/svg/close.svg"
 import hamburger from "../imgs/svg/hamburger.svg"
@@ -7,22 +7,63 @@ import hamburger from "../imgs/svg/hamburger.svg"
 import { Outlet, Link } from "react-router-dom";
 
 function Navbar() {
+  const [smallScreen, setSmallScreen] = useState(false);
   const [bar, setBar] = useState({ isHidden: false });
-
+  const style = { visibility: bar.isHidden ? 'hidden' : 'visible' };
 
   function toggleHidden() {
-      setBar({ isHidden: !bar.isHidden });
+    setBar({ isHidden: !bar.isHidden });
+  }
+  function toggleNav()
+  {
+    if(window.innerWidth <= 800)
+    {
+      toggleHidden();
+    }
+  }
+  function toggleOverflow()
+  {
+    //fix overflow in mobile navbar 
+    if(window.innerWidth <= 800 && !bar.isHidden)
+      document.body.style.overflow = "hidden";
+    else
+      document.body.style.overflow = "visible";
   }
 
-  const style = { visibility: bar.isHidden ? 'hidden' : 'visible' };
+  useEffect(()=>{
+    setBar({ isHidden: window.innerWidth <= 800 });
+  },[]);
+
+  useEffect(() => {
+    toggleOverflow();
+  }, );
+
+  useEffect(() => {
+    const resizeListener = () => {
+      toggleOverflow();
+      setSmallScreen(false);
+      if(window.innerWidth <= 800)
+      {
+        setSmallScreen(true);
+      }
+    };
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, []);
 
   return (
     <>
     <div className="navbar-container">
-    <h1>&#123;Club<span className="primary-color">Innovation</span>&#125;</h1>
+
+    <h1>&#123;Club<span className={smallScreen && !bar.isHidden ? 'secondary-color' : 'primary-color'}>Innovation</span>&#125;</h1>
+
       <div className="navbar">
         <nav className="navigation" style={style}>
-          <ul>
+          <ul onClick={toggleNav}>
             <li>
               <Link to="/">Home</Link>
             </li>
