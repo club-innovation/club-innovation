f69067fd677b0edc3271b652713c763182c5e9e2
unknown <badertebaa2@gmail.com>
2023-02-19 04:21:31 +0100

    footer not complete

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,11 +8,41 @@ function Footer() {
         <div className="container1">
             <div className="title1">want to become a part of our community?</div>
             <a href="" className="link1">Get in touch</a>
+<<<<<<< Updated upstream
             <h1>&#123;Club<span className="inov">Innovation</span>&#125;</h1>
           
         </div>
        </div>
 
+=======
+            <br/>
+            <br/>
+            <br/>
+            <div> <h2>&#123;Club <span className="inov">Innovation</span>&#125;</h2></div>
+            <br/>
+            <br/>
+            <div className="title2">want to become a part of us?</div>
+            <a href="" className="link1">check out our game challenge</a>
+          
+        </div>
+        <div className="container2">
+        <div className="title2">keep up with news from us</div>
+         <br/>
+         <input type={"text"} placeholder="Email" className="Email"/>
+         <br/>
+         <br/>
+         <br/>
+         
+         <input type={"checkbox" } className="check" /> <div className="signupText">Sign up to our Newsletter, to recieve updates on the latest news releases and special offers.</div> 
+           <br/>
+           <button className="Subscribe">Subscribe</button>
+
+        </div>
+        
+       </div>
+      
+      
+>>>>>>> Stashed changes
     </>
 
   )
