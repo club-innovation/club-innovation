1b8f295010016edc6f69c87e57448aaaa7f5f803
taha-ammour <122755276+taha-ammour@users.noreply.github.com>
2023-02-19 19:30:19 +0100

    optimized script and finished responsive

--- src/component/pages/Contact.js
+++ src/component/pages/Contact.js
@@ -13,7 +13,7 @@ function Contact() {
   const [message, setMessage] = useState("");
 
   function sendEmail() {
-    const recipient = "example@company.com";
+    const recipient = "example@company.com";//entrer l'email du club
     const subject = `Contact Us - ${firstName} ${lastName}`;
     const body = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phone}\nMessage: ${message}`;
     const messageUrl = `https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${recipient}&su=${encodeURIComponent(
@@ -33,68 +33,88 @@ function Contact() {
 
   return (
     <div className="Contact">
-      <p className="Info">Contact <span className="primary-color">Us</span></p>
+      
       <div className="ContainerContact">
-      <div className="ContainerContact1">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={firstName}
-            onChange={(event) => setFirstName(event.target.value)}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="phone">Phone Number:</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            value={phone}
-            onChange={(event) => setPhone(event.target.value)}
-            pattern="[0-9]*"
-            required
-          />
-          <br />
-          <br />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            required
-          />
-          <br />
-          <br />
-          <button type="submit">Contact Us</button>
-        </form>
+        <p className="Info">Contact <span className="primary-color">Us</span></p>
+        <div className="ContainerContact1">
+          <form onSubmit={handleSubmit}>
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+                required
+              />
+              <br />
+              <br />
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+                required
+              />
+              <br />
+              <br />
+              <label htmlFor="phone">Phone Number:</label>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+                pattern="[0-9]*"
+                required
+              />
+              <br />
+              <br />
+              <label htmlFor="message">Message:</label>
+              <textarea
+                id="message"
+                name="message"
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+                required
+              />
+              <br />
+              <br />
+              <div className="button">
+                <button type="submit">Contact Us</button>
+              </div>
+              
+            </form>
+          </div>
+          
+          
       </div>
-      <div className="ContainerContact2">
-        <p className="p1">Contact</p>
-        <p className="p2">contact@emsicoders.com</p>
-        <p className="p3">Based in</p>
-        <p className="p4">Marrakech</p>
-      </div>
-      <div className="ContainerContact3">
-      <a href="Facebook.com" className="facebook"><FacebookOutlinedIcon className="header-icon" fontSize="large" /></a>
-      <a href="Instagram.com" className="insta"><InstagramIcon className="header-icon" fontSize="large" /></a>
-      <a href="Twitter.com" className="twitter"><TwitterIcon className="header-icon" fontSize="large" /></a>
-
+      <div className="Container2">
+        <div className="Container21">
+          <div className="p1ragraph">
+            <p className="p1contain">Contact</p>
+            <p className="p2contain">contact@emsicoders.com</p>
+          </div>
+          <div className="p2ragraph">
+            <p className="p3contain">Based in</p>
+            <p className="p4contain">Marrakech</p>
+          </div>  
+        </div>    
+        <div className="ContainerContact3">
+          <ul>
+            <li> 
+              <a href="/" className="facebook"><FacebookOutlinedIcon className="iconp" fontSize="large" /></a>
+            </li>
+            <li>
+              <a href="/" className="insta"><InstagramIcon className="iconp" fontSize="large" /></a>
+           </li>
+            <li> 
+              <a href="/" className="twitter"><TwitterIcon className="iconp" fontSize="large" /></a>
+            </li>
+          </ul>
+           
         </div>
       </div>
     </div>
