f7136a235f087c817de42933ea04fd2b544c7306
taha-ammour <122755276+taha-ammour@users.noreply.github.com>
2023-02-19 02:08:40 +0100

    Update Footer.js

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,20 +3,7 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <> 
-       <div className="Footer">
-        <div className="container1">
-          <div className="parag">
-            <p className="p1">Want to become part of our community?</p>
-            <p className="p2">Get in touch</p>
-            <p className="p3"><h1>&#123;Club<span className="secondary-color">Innovation</span>&#125;</h1></p>
-            <p className="p4">Want to be part of Us?</p>
-            <p className="p5">Check out our game challenge</p>
-          </div>
-        </div>
-       </div>
-
-    </>
+    <div>Footer 2023</div>
 
   )
 }
