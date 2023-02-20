06edb20f5414b52854f85fe08736128c800ae5cf
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2023-02-20 02:52:45 +0100

    finish List component, Implement List into Projects and Events pages

--- src/component/pages/Events.js
+++ src/component/pages/Events.js
@@ -2,10 +2,14 @@ import React from "react"
 import "./style/Events.css"
 
 //Components
-import Description from "../Description";
+import Description from "../Description"
+import List from "../List"
 
 //Images
-import projectsSVG from "../../imgs/svg/calender.svg"
+import calenderSVG from "../../imgs/svg/calender.svg"
+
+//Data
+import EventsData from '../../data/EventsData'
 
 function Events() {
   return (
@@ -13,7 +17,11 @@ function Events() {
       <Description 
         title={<>Club <span className="primary-color">Innovation</span> Events</>} 
         description={"The main objective of Club Innovation at EMSI Marrakech is to foster innovation and creativity among the student body. To accomplish this, the club is responsible for organizing and hosting a variety of programming events, workshops, and competitions, aimed at encouraging students to develop and showcase their skills and talents."} 
-        image={projectsSVG}/>
+        image={calenderSVG}/>
+
+      <h2 className="list-title">&#123;Upcoming <span className="primary-color">events</span>&#125;</h2>  
+       
+      <List data={EventsData} page="events"/>
     </div>
   )
 }
