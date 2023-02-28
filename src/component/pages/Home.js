65ad4384a1e2a22357db8f9fd1c48c14b4589739
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-02-28 10:44:49 +0100

    Adding a functionality for scroll progress
    adding this functionality that is an alternative for the animation gif

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -38,6 +38,18 @@ function Home() {
       behavior: 'smooth'
     });
   }
+  const scrollListener = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollPercentage = (scrollTop / (scrollHeight - windowHeight)) * 100;
+    const progressValue = document.querySelector(".progress-value");
+    const progressScroll = document.querySelector(".progress");
+    progressValue.textContent = Math.round(scrollPercentage) + "%";
+    progressScroll.style.background = `conic-gradient(#8CEFE9 ${scrollPercentage}%, #070E21 ${scrollPercentage}%)`;
+  }
+  
+  window.addEventListener("scroll", scrollListener);
   return (
     <div>
       <Polygones/>
@@ -69,26 +81,25 @@ function Home() {
             </p>
             <input type="button" value="Explore"/>
           </div>
-          {isVisible &&<div className="toggle-up">
-            <img src={Up}/>
-          </div>}
+          
         </div>
-        
       </div>  
+     
       <div className="icons-left">
-        <div className="icons-container">
-          <Link target="blank_" to="https://discord.gg/ZB3GbCsG">
-            <span className="discord-icon" onClick={returnToHome}>
-              <img src={Discord}/>
-            </span>
-          </Link>
-          {isVisible && 
-            <div className="scroll-up">
-              <span onClick={returnToHome}><ArrowUpwardIcon fontSize="large" /></span>
-            </div>
-          }
-        </div>
+      <div className="icons-container">
+        <Link target="blank_" to="https://discord.gg/ZB3GbCsG">
+          <span className="discord-icon" onClick={returnToHome}>
+            <img src={Discord}/>
+          </span>
+        </Link>
+        {isVisible && 
+          <div className="progress">
+            
+            <span className="progress-value">0%</span>
+          </div>
+        }
       </div>
+    </div>
       <div className="club-information-container">
         <h2>
           &#123;What is Club<span className="primary-color">Innovation?</span>
