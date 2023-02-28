6e6faf05fab582bc8ca48130f17d4b26c3d129fc
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2023-02-28 20:26:35 +0100

    Updating all sections to add the icon and scroll progress

--- src/component/pages/About.js
+++ src/component/pages/About.js
@@ -1,17 +1,44 @@
-import React from "react"
 import "./style/About.css"
 
 //Components
 import Description from "../About_Description"
 import GoogleMap from "../GoogleMap"
 import { Polygones } from "../Particle"
-
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 //Images
 import teamwork from "../../imgs/AboutUs/AboutUs.gif"
 import Vision from "../../imgs/AboutUs/Vision.gif" 
 import build from "../../imgs/AboutUs/build.gif"
+import Discord from '../../imgs/home/discord.png'
 
 function About() {   
+   const [isVisible, setIsVisible] = useState(false);
+   function returnToHome() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }
+    
+    const scrollListener = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollPercentage = (scrollTop / (scrollHeight - windowHeight)) * 100;
+      const progressValue = document.querySelector(".progress-value");
+      const progressScroll = document.querySelector(".progress");
+      progressValue.textContent = Math.round(scrollPercentage) + "%";
+      progressScroll.style.background = `conic-gradient(#8CEFE9 ${scrollPercentage}%, #070E21 ${scrollPercentage}%)`;
+    }
+    window.addEventListener("scroll", scrollListener);
+    window.onscroll = function () {
+      if(window.scrollY >= 80) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false);
+      }
+    }
   return (
      <div className="about">
       <Polygones/>
@@ -42,6 +69,20 @@ function About() {
               <p>Our club innovation is situated in the vibrant city of <span className="primary-color"><b>Marrakech</b></span> at the intersection of Bv Hassan II and Rue Yougoslavie, We invite you to visit us at our location</p>
             </div>
         </div>
+        <div className="icons-left">
+      <div className="icons-container">
+        <Link target="blank_" to="https://discord.gg/ZB3GbCsG">
+          <span className="discord-icon" onClick={returnToHome}>
+            <img src={Discord}/>
+          </span>
+        </Link>
+        {isVisible && 
+          <div className="progress">
+            <span className="progress-value" onClick={returnToHome}>0%</span>
+          </div>
+        }
+      </div>
+    </div>
      </div>
   )
 }
